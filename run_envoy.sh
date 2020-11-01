d31f8aeaaa6d2b0c5fb45ea4818e3830b95c3549
philipz <philipzheng@gmail.com>
2020-11-01 17:15:09 +0800

    Fix error.

--- run_envoy.sh
+++ run_envoy.sh
@@ -1,1 +1,1 @@
-docker run -d --network=host -v /home/philipz/lab:/app getenvoy/envoy:stable --config-path /app/simple.yaml
+docker run -d --network=host -v $(pwd):/app getenvoy/envoy:stable --config-path /app/simple.yaml
--- stress_test.sh
+++ stress_test.sh
@@ -1,1 +1,1 @@
-./go-stress-testing-linux -c 2 -n 100 -d true -u http://localhost:8585/greeting
+./go-stress-testing-linux -c 2 -n 100 -u http://localhost:8585/greeting
